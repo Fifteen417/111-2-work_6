05c764a9d8edf9087eadb689e62376dac8882582
Fifteen417 <vannessgoh888@gmail.com>
2023-05-22 12:40:50 +0800

    12.40
    add function:
    Selection Font Family
    Selection Font Size
    change txt to rtf

--- work_6/work_6/MainWindow.xaml.cs
+++ work_6/work_6/MainWindow.xaml.cs
@@ -24,42 +24,54 @@ namespace work_6
         public MainWindow()
         {
             InitializeComponent();
+            // 清除rtbText的內容
+            text.Document.Blocks.Clear();
+            // 設定字型下拉選單的選單內容，存取你的電腦裡面的字型庫，將你安裝的字型清單都放進去
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            // 設定字體大小下拉選單的選單內容，設定8`72的數字，這要用來設定字體大小
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void open_btn_click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // 跟記事本範例程式類似，不過要改成過濾為RTF檔案格式
+            dlg.Filter = "RTF文件 (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(text.Document.ContentStart, text.Document.ContentEnd);
+                // DataFormats 檔案格式也要設定為RTF檔案格式
+                range.Load(fileStream, DataFormats.Rtf);
+            }
         }
 
         private void save_btn_click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-            // 設定檔案過濾，可以選擇只顯示純文字檔（*.txt）
-            dlg.Filter = "純文字資料 (*.txt)|*.txt|All files (*.*)|*.*";
-            // ShowDialog() 來顯示對話框，如果點選確認按鍵，會等於 true
+            dlg.Filter = "RTF文件 (*.rtf)|*.rtf|All files (*.*)|*.*";
             if (dlg.ShowDialog() == true)
             {
-                // 建立一個檔案資料流，並且設定檔案名稱與檔案開啟模式為「新增檔案」
                 FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
-                // 取得rtbText元件中文字的範圍，取得的範圍是「全部文字」
                 TextRange range = new TextRange(text.Document.ContentStart, text.Document.ContentEnd);
-                // 儲存檔案，並且設定為純文字文件檔案（*.txt）
-                range.Save(fileStream, DataFormats.Text);
-                // 關閉檔案資料流
-                fileStream.Close();
+                range.Save(fileStream, DataFormats.Rtf);
             }
         }
 
-        private void open_btn_click(object sender, RoutedEventArgs e)
+        private void cmbFontFamily_sc(object sender, SelectionChangedEventArgs e)
         {
-            // 開啟一個開啟檔案對話框
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.Filter = "純文字資料 (*.txt)|*.txt|All files (*.*)|*.*";
-            // ShowDialog() 來顯示對話框，如果點選開啟按鍵，會等於 true
-            if (dlg.ShowDialog() == true)
-            {
-                // 建立一個檔案資料流，並且設定檔案名稱與檔案開啟模式為「開啟檔案」
-                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
-                TextRange range = new TextRange(text.Document.ContentStart, text.Document.ContentEnd);
-                // 將檔案資料流以純文字格式，放進rtbText元件之中顯示
-                range.Load(fileStream, DataFormats.Text);
-                fileStream.Close();
-            }
+            // 判斷式：必須要有選擇項目，才會做文字格式改變
+            if (cmbFontFamily.SelectedItem != null)
+            // 將rtbText豐富文字框所選的項目，套用所設定的字型
+            text.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+
+        }
+
+        private void cmbFontSize_sc(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontSize.SelectedItem != null)
+            // 將rtbText豐富文字框所選的項目，套用所設定的字體大小
+            text.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.SelectedItem);
         }
     }
 }
